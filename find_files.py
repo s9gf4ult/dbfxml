a69bdd86e51446d0d5c7b708aeb24fe14f7d5df5
segfault <razor@calculate.local>
2010-09-24 16:39:54 +0000

    modified:   find_files.py 	робит

--- find_files.py
+++ find_files.py
@@ -1,4 +1,11 @@
 import re
+import os
 
-def findFilesByRegexp(directory, pattern):
-    
+def findFilesByRegexp(directory, pattern, flags = 0):
+    if os.path.exists(directory) and os.path.isdir(directory):
+        found = []
+        for dirpath, dirs, files in os.walk(directory):
+            found.extend(map(lambda a: "{0}/{1}".format(dirpath, a), filter(lambda a: re.match(pattern, a, flags = flags), files)))
+        return found
+
+          
