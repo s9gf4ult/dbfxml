0d17de184b2c3d4532b7fc12900d00ce01d8e6a2
segfault <razor@calculate.local>
2010-09-24 12:50:57 +0000

    transformer.py
    класс для транформации мутоты

---
+++ transformer.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+
+class transformer:
+    def __init__(self, sq_connection):
+        if sq_connection.__class__ != sqlite3.Connection:
+            raise Exception("sq_connection is not sqlite3.Connection instance")
+
+    
