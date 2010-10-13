c9dcff28196035af7c9fd2f30ae3da2ce3213c7e
segfault <razor@calculate.local>
2010-10-13 17:25:34 +0000

    modified:   common_helpers.py 	deleted:    find_files.py

--- core/common_helpers.py
+++ core/common_helpers.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+import os
 
 def declare_type(type, *vars):
     for var in vars:
@@ -9,3 +11,12 @@ def declare_class(type, *classes):
     for classname in classes:
         if not issubclass(classname, type):
             raise Exception(u'{0}: {1} is not subclass of {2}'.format(__name__, classname.__name__, type.__name__))
+
+def findFilesByRegexp(directory, pattern, flags = 0):
+    if os.path.exists(directory) and os.path.isdir(directory):
+        found = []
+        for dirpath, dirs, files in os.walk(directory):
+            found.extend(map(lambda a: "{0}/{1}".format(dirpath, a), filter(lambda a: re.match(pattern, a, flags = flags), files)))
+        return found
+
+          
